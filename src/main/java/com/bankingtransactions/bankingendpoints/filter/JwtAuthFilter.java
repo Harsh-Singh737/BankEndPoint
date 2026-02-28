8acfb46cbe90a6780a1e248d9890bd9bb2880e02
Harsh Singh <harshsinghlo737@gmail.com>
2026-02-28 13:15:56 +0530

    added Nonnull annotation=

--- src/main/java/com/bankingtransactions/bankingendpoints/filter/JwtAuthFilter.java
+++ src/main/java/com/bankingtransactions/bankingendpoints/filter/JwtAuthFilter.java
@@ -1,11 +1,9 @@
 package com.bankingtransactions.bankingendpoints.filter;
 
 import com.bankingtransactions.bankingendpoints.uitils.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Nonnull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -27,8 +25,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+                                    @Nonnull HttpServletResponse response,
+                                    @Nonnull FilterChain filterChain)
             throws ServletException, IOException {
 
         String path = request.getRequestURI();
