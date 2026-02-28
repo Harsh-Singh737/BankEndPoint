832eb09c9d0fd5a862b525299e5516e9554c07a6
Harsh Singh <harshsinghlo737@gmail.com>
2026-02-28 13:24:16 +0530

    checking it

--- src/main/java/com/bankingtransactions/bankingendpoints/filter/JwtAuthFilter.java
+++ src/main/java/com/bankingtransactions/bankingendpoints/filter/JwtAuthFilter.java
@@ -1,9 +1,11 @@
 package com.bankingtransactions.bankingendpoints.filter;
 
 import com.bankingtransactions.bankingendpoints.uitils.JwtUtil;
-import jakarta.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -25,8 +27,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    @Nonnull HttpServletResponse response,
-                                    @Nonnull FilterChain filterChain)
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
             throws ServletException, IOException {
 
         String path = request.getRequestURI();
