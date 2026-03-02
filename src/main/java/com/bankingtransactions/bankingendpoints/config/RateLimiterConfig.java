807f80138fe3d4294ff8d8db7ec8a491c5c10506
Harsh Singh <harshsinghlo737@gmail.com>
2026-03-02 13:47:53 +0530

    some update in rate limit config

--- src/main/java/com/bankingtransactions/bankingendpoints/config/RateLimiterConfig.java
+++ src/main/java/com/bankingtransactions/bankingendpoints/config/RateLimiterConfig.java
@@ -3,22 +3,28 @@ package com.bankingtransactions.bankingendpoints.config;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class RateLimiterConfig {
 
-    private final Bucket bucket;
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
 
-    public RateLimiterConfig() {
-        Bandwidth limit = Bandwidth.simple(5, Duration.ofMinutes(1));
-        this.bucket = Bucket4j.builder().addLimit(limit).build();
+    public Bucket resolveBucket(String key) {
+        return cache.computeIfAbsent(key, k -> newBucket());
     }
 
-    public Bucket getBucket() {
-        return bucket;
+    private Bucket newBucket() {
+        Refill refill = Refill.greedy(5, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(5, refill);
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 }
